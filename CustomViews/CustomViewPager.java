58fd02196e99f706ab110893d7e1970259051589
muthu1001 <muthukumarmanoharan1001@gmail.com>
2019-12-07 08:24:53 +0530

    Add files via upload

---
+++ CustomViews/CustomViewPager.java
@@ -0,0 +1,41 @@
+package com.thanthi.dtnext.dtnextapplication.widget;
+
+import android.content.Context;
+import androidx.viewpager.widget.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomViewPager extends ViewPager {
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs){
+
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+
+            View child = getChildAt(i);
+
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+
+            int h = child.getMeasuredHeight();
+
+            if(h > height) height = h;
+
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
